dd8e16e566e150166c60ed21ba269ec30c200b70
webgeekhabib007 <73267589+webgeekhabib007@users.noreply.github.com>
2022-12-18 01:31:45 +0600

    Create False_position_method.cpp

---
+++ False_position_method.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+#define f(x) cos(x) - x * exp(x)
+
+using namespace std;
+
+int main()
+{
+      float x0, x1, x, f0, f1, f, e;
+      int step = 1;
+
+     cout<< setprecision(6)<< fixed;
+
+      up:
+      cout<<"Enter first guess: ";
+     cin>>x0;
+     cout<<"Enter second guess: ";
+     cin>>x1;
+     cout<<"Enter tolerable error: ";
+     cin>>e;
+
+      f0 = f(x0),f1 = f(x1);
+      if( f0 * f1 > 0.0)
+      {
+            cout<<"Incorrect Initial Guesses."<< endl;
+            goto up;
+      }
+     cout<< endl<<"*********************"<< endl;
+      cout<<"False Position Method"<< endl;
+      cout<<"*********************"<< endl;
+      do
+      {
+            x = x0 - (x0-x1) * f0/ (f0-f1);
+            f = f(x);
+
+            cout<<"Iteration-"<< step<<":\t x = "<< setw(10)<< x<<" and f(x) = "<< setw(10)<< f(x)<< endl;
+
+            if( f0 * f < 0){
+                  x1 = x;
+                  f1 = f;
+            }
+            else{
+                  x0 = x;
+                  f0 = f;
+            }
+            step = step + 1;
+      }while(fabs(f)>e);
+
+      cout<< endl<<"Root is: "<< x<< endl;
+
+      return 0;
+}
+
+//guesses for this function are : 0, 1
+//margin of error: .0000001
